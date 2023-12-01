965c141172653cd81e3fc04039e21a7f90f40bc8
panypham <panypham39@gmail.com>
2023-11-30 21:10:15 -0600

    first commit

---
+++ lib/quiz.dart
@@ -0,0 +1,44 @@
+import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 50, 13, 112),
+              Color.fromARGB(255, 110, 91, 144),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
