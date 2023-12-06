3fe4c4bc0dec09738223b61b067e99c7341321eb
panypham <panypham39@gmail.com>
2023-12-05 22:11:56 -0600

    Refactor code

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,7 +4,7 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 
-  List<String> getShuffleAnswers() {
+  List<String> get shuffleAnswers {
     final shuffledList = List.of(answers);
     shuffledList.shuffle();
     return shuffledList;
